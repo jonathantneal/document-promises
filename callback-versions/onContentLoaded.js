72d411a83d97ea3070cc89ad9aabc85919983e8d
Jonathan Neal <jonathantneal@hotmail.com>
2018-08-10 07:12:03 -0400

    4.0.0

--- callback-versions/onContentLoaded.js
+++ callback-versions/onContentLoaded.js
@@ -1,4 +1,4 @@
-export default (c) => !function d() {
+export default c => !function d() {
 	/c/.test(document.readyState) && document.body
 	? document.removeEventListener("DOMContentLoaded", d) | c()
 	: document.addEventListener("DOMContentLoaded", d)
--- callback-versions/onLoaded.js
+++ callback-versions/onLoaded.js
@@ -1,4 +1,4 @@
-export default (c) => !function d() {
+export default c => !function d() {
 	/m/.test(document.readyState)
 	? document.removeEventListener("readystatechange", d) | c()
 	: document.addEventListener("readystatechange", d)
